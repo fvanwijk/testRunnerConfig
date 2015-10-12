bf2d7e178d7b27f9454d7b7159ddb6358c16fc40
Frank van Wijk <fvanwijk@xebia.com>
2015-10-12 16:48:22 +0200

    Added support for exclude pattern in Karma

--- src/index.js
+++ src/index.js
@@ -16,6 +16,15 @@ var notInstrument = function (file) {
   };
 };
 
+function applyMappings(mappings) {
+  return function (group) {
+    return {
+      type: group.type,
+      files: group.files.map(mappings[group.type] || _.identity)
+    };
+  }
+}
+
 function getWallabyFiles(files, mappings) {
   mappings = _.defaults(mappings || {}, {
     config: notInstrument,
@@ -40,12 +49,12 @@ function getWallabyFiles(files, mappings) {
     src: instrument
   });
 
-  var wallabyFiles = files.map(function (group) {
-    return {
-      type: group.type,
-      files: group.files.map(mappings[group.type])
-    };
-  });
+  // Add 'specs' list to files as 'ignored'. If we had added them manually to the ignore list, Karma has no specs to run.
+  var specs = _.findWhere(files, { type: 'specs' }).files;
+  var ignoreIndex = _.findIndex(files, { type: 'ignore' });
+  files[ignoreIndex].files = files[ignoreIndex].files.concat(specs);
+
+  var wallabyFiles = files.map(applyMappings(mappings));
 
   return {
     files: _(wallabyFiles)
@@ -58,15 +67,26 @@ function getWallabyFiles(files, mappings) {
   };
 }
 
-function getKarmaFiles(files) {
+function getKarmaFiles(files, mappings) {
+  mappings = _.defaults(mappings || {}, {
+    mock: function (file) {
+      return {
+        pattern: file,
+        included: false
+      };
+    }
+  });
+
+  var karmaFiles = files.map(applyMappings(mappings));
+
   return {
-    files: _(files)
+    files: _(karmaFiles)
       .reject({ type: 'ignore' })
       .pluck('files')
       .values()
       .flatten()
       .value(),
-    exclude: _.result(_.findWhere(files, { type: 'ignore' }), 'files')
+    exclude: _.result(_.findWhere(karmaFiles, { type: 'ignore' }), 'files')
   };
 }
 
