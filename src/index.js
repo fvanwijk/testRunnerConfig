341e723deb2e7c7b3bb8662c3a169fad8c17d58d
Frank van Wijk <fvanwijk@xebia.com>
2015-10-26 23:16:59 +0100

    Fix bug when there is no files list for type spec

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function getWallabyFiles(files, mappings) {
 
   // Add 'specs' list to files as 'ignored'. If we had added them manually to the ignore list, Karma has no specs to run.
   var ignoreIndex = _.findIndex(files, { type: 'ignore' });
-  var specs = _.findWhere(files, { type: 'specs' }).files;
+  var specs = _.result(_.findWhere(files, { type: 'specs' }), 'files', []);
   if (files[ignoreIndex]) {
     files[ignoreIndex].files = files[ignoreIndex].files.concat(specs);
   } else {
