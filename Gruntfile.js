d4ca0fd3780cdc4724da3e82c71062a78dcd1aee
Frank van Wijk <fvanwijk@xebia.com>
2016-02-14 16:38:51 +0100

    Clean up gruntfile and update dependencies. Use new way of depending on Babel 6

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,34 +5,37 @@ var testFiles = require('./src/').getMochaFiles(require('./test/testFiles.js'));
 module.exports = function (grunt) {
 
   require('load-grunt-tasks')(grunt);
-  require('time-grunt')(grunt);
   grunt.loadNpmTasks('grunt-mocha-istanbul');
   grunt.task.renameTask('mocha_istanbul', 'mochaIstanbul');
 
-  var appConfig = {
+  var paths = {
     src: 'src',
     dist: 'dist',
     test: 'test'
   };
 
   grunt.initConfig({
-
-    paths: appConfig,
-
-    watch: {
-      js: {
-        files: ['<%= paths.src %>/**/*.js', 'Gruntfile.js'],
-        tasks: ['newer:jshint:src', 'newer:jscs:src']
+    jscs: {
+      options: {
+        config: './.jscsrc'
+      },
+      src: {
+        src: paths.src + '/**/*.js'
+      },
+      test: {
+        src: testFiles
+      },
+      config: {
+        src: '*.js'
       }
     },
-
     jshint: {
       options: {
         jshintrc: '.jshintrc',
         reporter: require('jshint-stylish')
       },
       src: {
-        src: '<%= paths.src %>/**/*.js'
+        src: paths.src + '/**/*.js'
       },
       test: {
         src: testFiles
@@ -41,39 +44,12 @@ module.exports = function (grunt) {
         src: '*.js'
       }
     },
-
-    jscs: {
-      options: {
-        config: './.jscsrc'
-      },
-      src: {
-        src: '<%= paths.src %>/**/*.js'
-      },
-      test: {
-        src: testFiles
-      },
-      config: {
-        src: '*.js'
-      }
-    },
-
-    mochaTest: {
-      // Run without Grunt:
-      // mocha test/specs --require babel/register
-      test: {
-        options: {
-          require: 'babel/register'
-        },
-        src: testFiles
-      }
-    },
-
     mochaIstanbul: {
       coverage: {
         src: testFiles,
         options: {
-          coverageFolder: 'test/coverage',
-          mochaOptions: ['--require=babel/register'],
+          coverageFolder: paths.test + '/coverage',
+          mochaOptions: ['--require=babel-register'],
           check: {
             lines: 100,
             statements: 100,
@@ -83,13 +59,14 @@ module.exports = function (grunt) {
         }
       }
     },
-
-    bump: {
-      options: {
-        files: ['package.json'],
-        commitFiles: ['package.json'],
-        commitMessage: 'Bump version to v%VERSION%',
-        push: false
+    mochaTest: {
+      // Run without Grunt:
+      // mocha test/specs --require babel-register
+      test: {
+        options: {
+          require: 'babel-register'
+        },
+        src: testFiles
       }
     }
   });
